d036042573ad0ed4734b3dcb27aca837b963408f
libowei <libowei123123@qq.com>
2016-04-24 16:44:37 +0800

    可以保存每天的事件文件

--- today.py
+++ today.py
@@ -2,7 +2,8 @@
 
 import re
 import urllib
-import time
+import datetime
+import os
 
 def getHtml(url):
     page = urllib.urlopen(url)
@@ -10,22 +11,54 @@ def getHtml(url):
     return html
     
 
-def processStr(str):
-    return re.compile('<[\s\S]+?>').sub('',str)
+def getList():
     
+    list = []
+    date = datetime.date(2016,1,1)
+    i = 1
+    while i <= 366:
+       date_str = date.strftime('%m月%d日')
+       list.append(date_str)
+       date += datetime.timedelta(days=1)
+       i += 1
+    return list
     
-def main():    
-    date_str=time.strftime("%m月%d日",time.localtime())
-    base_url="https://zh.wikipedia.org/zh-cn/%s"%date_str
+def processHtml(html):
+    reg_list=[]
+    reg_list.append('<[\s\S]+?>') #标签
+    for reg in reg_list:
+        html = re.compile(reg).sub('',html)    
+    return html.split('\n');   
+        
     
-    html=getHtml(base_url)
+def saveFile(date,content):
+    file = open('data/' + date + '.txt', 'a')
+    file.write(content)
+    file.close()
     
-    reg=r'</h2>\s+?<ul>([\s\S]+?)<h2>'
-    html_list=re.findall(re.compile(reg),html)
-    
-    for html_str in html_list:
-        print processStr(html_str)
-    
+def main():
+
+    list = getList()
+    for str in list:
+        url="https://zh.wikipedia.org/zh-cn/%s"%str
+        html=getHtml(url)
+        reg=r'</h2>\s+?<ul>([\s\S]+?)<h2>'
+        html_list=re.findall(re.compile(reg),html)
+        year=''
+        for line in processHtml(html_list[0]):
+            if line.strip()=='':
+                continue
+            match = re.compile(r'^[前]?\d{1,4}年：').match(line)
+            if match:
+                year = match.group()
+            else:
+                line = year + line
+            line = line.replace(year,'',1)
+            if line.strip()=='':
+                continue
+            line = year + line + '\n'
+            saveFile(str,line)
+        print str
     
 
 if __name__=="__main__":
